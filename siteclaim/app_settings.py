ba543bc9540384888fd0bea22ac7461548943b53
Aaron Kable <aaronkable@gmail.com>
2023-04-29 10:42:40 +0800

    add options for each cog

--- siteclaim/app_settings.py
+++ siteclaim/app_settings.py
@@ -1,1 +1,4 @@
 from django.conf import settings
+
+SITE_CLAIM_ENABLE_ESS = getattr(settings, 'SITE_CLAIM_ENABLE_ESS', True)
+SITE_CLAIM_ENABLE_SITES = getattr(settings, 'SITE_CLAIM_ENABLE_SITES', True)
--- siteclaim/auth_hooks.py
+++ siteclaim/auth_hooks.py
@@ -1,6 +1,13 @@
 from allianceauth import hooks
 
+from .app_settings import SITE_CLAIM_ENABLE_ESS, SITE_CLAIM_ENABLE_SITES
+
 
 @hooks.register('discord_cogs_hook')
 def register_cogs():
-    return ['siteclaim.cogs.sites', 'siteclaim.cogs.ess']
+    cogs = []
+    if SITE_CLAIM_ENABLE_SITES:
+        cogs.append('siteclaim.cogs.sites')
+    if SITE_CLAIM_ENABLE_ESS:
+        cogs.append('siteclaim.cogs.ess')
+    return cogs
